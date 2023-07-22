88712bca3576a4fbec0d6a1bf86a3502c7d722cb
youtube <youtube@example.com>
2023-07-21 22:32:32 -0500

    basic setup, udemy module 237

--- BulkyWeb/Program.cs
+++ BulkyWeb/Program.cs
@@ -1,7 +1,10 @@
+using BulkyBookWeb.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer);
 
 var app = builder.Build();
 
